b12d3780998681bb0a3a471b801f1f4140900e06
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2025-02-15 01:37:05 +0200

    Repeat commit

--- front-end/script.js
+++ front-end/script.js
@@ -1,43 +1,51 @@
-// script.js
+const url = 'http://localhost:8000/api/contacts/'
 
-const url = 'http://localhost:8000/api/todos/'
-
-window.getTasks = async function (skip = 0, limit = 100) {
+window.getContacts = async function (skip = 0, limit = 100) {
   const response = await fetch(`${url}?skip=${skip}&limit=${limit}`)
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`)
   }
-  const tasks = await response.json()
-  return tasks
+  const contacts = await response.json()
+  return contacts
 }
 
-window.createTask = async function (task) {
-  const response = await fetch(`${url}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(task),
-  })
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`)
+window.createContact = async function (contact) {
+  try {
+    const response = await fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(contact),
+    })
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail[0].msg);
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
-window.editTask = async function (task) {
-  const response = await fetch(`${url}${task.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(task),
-  })
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`)
+window.editContact = async function (contact) {
+  try {
+    const response = await fetch(`${url}${contact.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(contact),
+    })
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail[0].msg);
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
-window.deleteTask = async function (id) {
+window.deleteContact = async function (id) {
   const response = await fetch(`${url}${id}`, {
     method: 'DELETE',
   })
